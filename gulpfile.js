94d672096fb60e9972016e5aca94be759d07b10b
lin <469946668@qq.com>
2019-03-10 18:07:49 +0800

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require("gulp");
 // const terser = require("gulp-terser");
 // const rename = require("gulp-rename");
 
-//bootstrap-toasts  &&  jquery
+//bootstrap-toasts
 const bootstrap_toasts_js_path = "./node_modules/bootstrap-toasts/dist/bootstrap-toasts.js";
 const bootstrap_toasts_min_js_path = "./node_modules/bootstrap-toasts/dist/bootstrap-toasts.min.js";
+
+//jquery
 const jquery_js_path = "./node_modules/jquery/dist/jquery.js";
 const jquery_min_js_path = "./node_modules/jquery/dist/jquery.min.js";
 
@@ -33,10 +35,19 @@ const font_css_path = "./node_modules/@fortawesome/fontawesome-free/css/all.css"
 const font_min_css_path = "./node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 const font_webfonts_path = "./node_modules/@fortawesome/fontawesome-free/webfonts/*";
 
-gulp.task("copy_bs", copy_bs);
-gulp.task("copy_js", copy_js);
+//video.js
+const video_js_path = "./node_modules/video.js/dist/video.js";
+const video_min_js_path = "./node_modules/video.js/dist/video.min.js";
+const video_css_path = "./node_modules/video.js/dist/video-js.css";
+const video_min_css_path = "./node_modules/video.js/dist/video-js.min.css";
+
+gulp.task("copy_jq", copy_jq);
 gulp.task("copy_popper", copy_popper);
+gulp.task("copy_bs", copy_bs);
+gulp.task("copy_bt", copy_bt);
+gulp.task("copy_vue", copy_vue);
 gulp.task("copy_fonts", copy_fonts);
+gulp.task("copy_video", copy_video);
 
 function copy_fonts(done) {
     gulp.src([font_css_path, font_min_css_path]).pipe(gulp.dest("./static/font/css"));
@@ -45,21 +56,33 @@ function copy_fonts(done) {
 }
 
 function copy_bs(done) {
-    gulp.src([bs_js_path, bs_min_js_path, bs_js_map_path, bs_min_js_map_path])
-        .pipe(gulp.dest("./static/js/"));
-    gulp.src([bs_css_path, bs_min_css_path, bs_css_map_path, bs_min_css_map])
-        .pipe(gulp.dest("./static/css/"));
+    gulp.src([bs_js_path, bs_min_js_path, bs_js_map_path, bs_min_js_map_path]).pipe(gulp.dest("./static/js/"));
+    gulp.src([bs_css_path, bs_min_css_path, bs_css_map_path, bs_min_css_map]).pipe(gulp.dest("./static/css/"));
     done();
 }
 
 function copy_popper(done) {
-    gulp.src([popper_js_path, popper_min_js_path, popper_js_map_path, popper_min_js_map_path])
-        .pipe(gulp.dest("./static/js/"));
+    gulp.src([popper_js_path, popper_min_js_path, popper_js_map_path, popper_min_js_map_path]).pipe(gulp.dest("./static/js/"));
     done();
 }
 
-function copy_js(done) {
-    gulp.src([bootstrap_toasts_js_path, bootstrap_toasts_min_js_path, jquery_js_path, jquery_min_js_path, vue_js_path, vue_min_js_path])
-        .pipe(gulp.dest("./static/js/"));
+function copy_bt(done) {
+    gulp.src([bootstrap_toasts_js_path, bootstrap_toasts_min_js_path]).pipe(gulp.dest("./static/js/"));
+    done();
+}
+
+function copy_jq(done) {
+    gulp.src([jquery_js_path, jquery_min_js_path]).pipe(gulp.dest("./static/js/"));
+    done();
+}
+
+function copy_vue(done) {
+    gulp.src([vue_js_path, vue_min_js_path]).pipe(gulp.dest("./static/js/"));
+    done();
+}
+
+function copy_video(done) {
+    gulp.src([video_js_path, video_min_js_path]).pipe(gulp.dest("./static/js/"));
+    gulp.src([video_css_path, video_min_css_path]).pipe(gulp.dest("./static/css/"));
     done();
 }
