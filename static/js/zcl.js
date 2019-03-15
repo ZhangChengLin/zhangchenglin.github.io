ed0b0bee41dd855b09f08b2a8a576687c5a6dc2f
lin <469946668@qq.com>
2019-03-15 09:44:26 +0800

    Initial commit

---
+++ static/js/zcl.js
@@ -0,0 +1,16 @@
+(function () {
+    const hitokoto = document.querySelector("#hitokoto");
+    hitokoto.addEventListener("click", function () {
+        fetch('https://v1.hitokoto.cn')
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                const hitokoto = document.querySelector('#hitokoto');
+                hitokoto.innerText = data.hitokoto;
+            })
+            .catch(function (err) {
+                console.error(err);
+            })
+    })
+})();
