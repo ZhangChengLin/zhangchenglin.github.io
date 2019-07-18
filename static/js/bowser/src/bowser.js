d8aa39e10178a7d6c7de0fa5fdde3b8b94e994dc
zhangchenglin <469946668@qq.com>
2019-07-18 15:31:55 +0800

    Initial commit

--- static/js/bowser/src/bowser.js
+++ static/js/bowser/src/bowser.js
@@ -2,9 +2,15 @@
  * Bowser - a browser detector
  * https://github.com/lancedikson/bowser
  * MIT License | (c) Dustin Diaz 2012-2015
- * MIT License | (c) Denis Demchenko 2015-2017
+ * MIT License | (c) Denis Demchenko 2015-2019
  */
 import Parser from './parser.js';
+import {
+  BROWSER_MAP,
+  ENGINE_MAP,
+  OS_MAP,
+  PLATFORMS_MAP,
+} from './constants.js';
 
 /**
  * Bowser class.
@@ -12,13 +18,18 @@ import Parser from './parser.js';
  * It's supposed to work with collections of {@link Parser} instances
  * rather then solve one-instance problems.
  * All the one-instance stuff is located in Parser class.
+ *
+ * @class
+ * @classdesc Bowser is a static object, that provides an API to the Parsers
+ * @hideconstructor
  */
 class Bowser {
   /**
-   * Creates a {@link module:parser:Parser} instance
+   * Creates a {@link Parser} instance
    *
    * @param {String} UA UserAgent string
-   * @param {Boolean} [skipParsing=false] same as skipParsing for {@link Parser}
+   * @param {Boolean} [skipParsing=false] Will make the Parser postpone parsing until you ask it
+   * explicitly. Same as `skipParsing` for {@link Parser}.
    * @returns {Parser}
    * @throws {Error} when UA is not a String
    *
@@ -44,6 +55,22 @@ class Bowser {
    */
   static parse(UA) {
     return (new Parser(UA)).getResult();
+  }
+
+  static get BROWSER_MAP() {
+    return BROWSER_MAP;
+  }
+
+  static get ENGINE_MAP() {
+    return ENGINE_MAP;
+  }
+
+  static get OS_MAP() {
+    return OS_MAP;
+  }
+
+  static get PLATFORMS_MAP() {
+    return PLATFORMS_MAP;
   }
 }
 
